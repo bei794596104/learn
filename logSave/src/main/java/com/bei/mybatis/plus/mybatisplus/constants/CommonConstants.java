bd79555b54ca3da42fadccb8a33ee2fff3423c9b
bei794596104 <794596104@qq.com>
2020-01-15 11:39:16 +0800

    aop结合springEvent异步记录操作日志

---
+++ logSave/src/main/java/com/bei/mybatis/plus/mybatisplus/constants/CommonConstants.java
@@ -0,0 +1,22 @@
+package com.bei.mybatis.plus.mybatisplus.constants;
+
+/**
+ * @author bei
+ * @2020/1/13 14:55
+ */
+public interface CommonConstants {
+    /**
+     * 成功标记
+     */
+    Integer SUCCESS = 0;
+    /**
+     * 失败标记
+     */
+    Integer FAIL = 1;
+
+    /**
+     * 正常
+     */
+    String STATUS_NORMAL = "0";
+
+}
