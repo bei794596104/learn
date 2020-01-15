a367c2e7589c7294564b90304c33aefc801d3aef
bei794596104 <794596104@qq.com>
2020-01-15 10:30:09 +0800

    springboot整合mybatisPlus

---
+++ mybatis-plus/src/main/java/com/bei/mybatis/plus/mybatisplus/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.bei.mybatis.plus.mybatisplus.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.bei.mybatis.plus.mybatisplus.service.UserService;
+import com.bei.mybatis.plus.mybatisplus.utils.R;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bei
+ * @2020/1/13 14:53
+ */
+@RestController
+@RequestMapping("/user")
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/page")
+    public R getUserList(){
+        return R.ok(userService.list());
+    }
+
+
+}
+
